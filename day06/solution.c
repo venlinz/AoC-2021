8f491f63d70ed0b98e767484c36119f11d662e58
venkateshht <shariharanvenkatesh@gmail.com>
2021-12-22 16:17:38 +0000

    written a faster algorithm to find fishes with o(1) space and o(n) time complexity 'n' is no of days

--- day06/solution.c
+++ day06/solution.c
@@ -1,45 +1,75 @@
 #include <stdio.h>
 
-#define NO_DAYS 18
+#define NO_DAYS 256
 #define MAX_HATCH_DELAY 8
 #define ARR_LEN(x) (sizeof(x) / sizeof(x[0]))
 
+void print_fish(long *fishes_days);
 
 int main(void)
 {
     long fishes_days[MAX_HATCH_DELAY + 1] = {0};
-    (void) fishes_days;
-    int input[] = {1, 2, 3, 3, 4};
+    int input[] = {
+        1,5,5,1,5,1,5,3,1,3,2,4,3,4,1,1,3,5,4,4,2,1,
+        2,1,2,1,2,1,5,2,1,5,1,2,2,1,5,5,5,1,1,1,5,1,
+        3,4,5,1,2,2,5,5,3,4,5,4,4,1,4,5,3,4,4,5,2,4,
+        2,2,1,3,4,3,2,3,4,1,4,4,4,5,1,3,4,2,5,4,5,3,
+        1,4,1,1,1,2,4,2,1,5,1,4,5,3,3,4,1,1,4,3,4,1,
+        1,1,5,4,3,5,2,4,1,1,2,3,2,4,4,3,3,5,3,1,4,5,
+        5,4,3,3,5,1,5,3,5,2,5,1,5,5,2,3,3,1,1,2,2,4,
+        3,1,5,1,1,3,1,4,1,2,3,5,5,1,2,3,4,3,4,1,1,5,
+        5,3,3,4,5,1,1,4,1,4,1,3,5,5,1,4,3,1,3,5,5,5,
+        5,5,2,2,1,2,4,1,5,3,3,5,4,5,4,1,5,1,5,1,2,5,
+        4,5,5,3,2,2,2,5,4,4,3,3,1,4,1,2,3,1,5,4,5,3,
+        4,1,1,2,2,1,2,5,1,1,1,5,4,5,2,1,4,4,1,1,3,3,
+        1,3,2,1,5,2,3,4,5,3,5,4,3,1,3,5,5,5,5,2,1,1,
+        4,2,5,1,5,1,3,4,3,5,5,1,4,3
+    };
     size_t input_len = ARR_LEN(input);
-    /* printf("array_len: %lu\n", */ 
     for (size_t i = 0; i < input_len; ++i)
     {
         fishes_days[input[i]]++;
     }
-    printf("fishes_days: \n");
-    for (int i = 0; i < MAX_HATCH_DELAY + 1; ++i)
+
+    printf("init fishes_days: \n");
+    print_fish(fishes_days);
+
+    size_t counter = 0;
+    for (size_t i = 0; i < NO_DAYS; ++i, counter++)
+    {
+        long fishes_days0 = fishes_days[0];
+        fishes_days[0] -= fishes_days0;
+        for (size_t j = 1; j < MAX_HATCH_DELAY + 1; ++j)
+        {
+            long fishes_day_cur = fishes_days[j];
+            fishes_days[j - 1] += fishes_day_cur;;
+            fishes_days[j] -= fishes_day_cur;
+        }
+        fishes_days[6] += fishes_days0;
+        fishes_days[8] += fishes_days0;
+    }
+
+    printf("counter: %ld\n", counter);
+    printf("\noutput: \n");
+    print_fish(fishes_days);
+    long sum = 0;
+    for (int i = 0; i < 9; i++)
+    {
+        sum += fishes_days[i];
+    }
+    printf("\nanswer: %ld\n", sum);
+    return 0;
+}
+
+void print_fish(long *fishes_days)
+{
+    for (int i = 0; i < 9; i++)
+    {
+        printf("%d ", i);
+    }
+    printf("\n");
+    for (int i = 0; i < 9; i++)
     {
         printf("%ld ", fishes_days[i]);
     }
-    printf("\n");
-    for (size_t i = 0; i < NO_DAYS; ++i)
-    {
-        if (fishes_days[0] > 0)
-        {
-            long fishes_days0 = fishes_days[0];
-            fishes_days[0] = 0;
-            fishes_days[6] += fishes_days0;
-            fishes_days[8] += fishes_days0;
-            printf("d 6: %ld\n", fishes_days[6]);
-        }
-        else
-        {
-            for (size_t j = 1; j < MAX_HATCH_DELAY; ++j)
-            {
-                fishes_days[j - 1] += fishes_days[j];
-                fishes_days[j] -= fishes_days[j];
-            }
-        }
-    }
-    return 0;
 }
